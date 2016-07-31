bb8059f1e78086e4e52234896cb46919d4d8ddb9
George Jenkins <g.jenkins@live.com>
2016-07-30 20:27:44 -0700

    Style update

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,30 @@ var browserSync = require('browser-sync').create();
 
 gulp.task('default', ['serve']);
 
-// Static Server + watching sass/html files
 gulp.task('serve', ['minify-css'], function() {
-
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-
-    gulp.watch("./src/**/*.sass", ['sass']); //If any sass changes, then process them
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  
+  gulp.watch("./src/**/*.sass", ['sass']);
 });
 
 gulp.task('sass', function() {
-    return gulp.src("./src/legacy.sass") 		//Grab the top level sass files
-        .pipe(sass())					//Process them into CSS
-        .on('error', onError)			//Print error if one occurs
-        .pipe(autoprefixer())			//Prefix all necessary items
-        .pipe(gulp.dest("./dist/"))	;	//Pipe to file
-
+  return gulp.src("./src/legacy.sass")
+  .pipe(sass())
+  .on('error', onError)
+  .pipe(autoprefixer())
+  .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task('minify-css', ['sass'], function() {
-	return gulp.src("./dist/legacy.css")
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest("./dist/"))	
-    .pipe(browserSync.stream());	//Stream changes to all browsers
+  return gulp.src("./dist/legacy.css")
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest("./dist/"))	
+  .pipe(browserSync.stream());
 });
 
 function onError(err) {
